396d7be37ce161148c44964215df35f290debf13
rock <rok@gmail.com>
2025-01-13 22:32:41 +0530

    added  feign client and small code refactoring

--- gym-web-project1/src/main/java/com/example/gym_web_project1/GymWebProject1Application.java
+++ gym-web-project1/src/main/java/com/example/gym_web_project1/GymWebProject1Application.java
@@ -2,8 +2,10 @@ package com.example.gym_web_project1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GymWebProject1Application {
 
 	public static void main(String[] args) {
--- gym-web-project1/src/test/java/com/example/gym_web_project1/adapters/in/web/GymControllerTest.java
+++ gym-web-project1/src/test/java/com/example/gym_web_project1/adapters/in/web/GymControllerTest.java
@@ -3,19 +3,15 @@ package com.example.gym_web_project1.adapters.in.web;
 import com.example.gym_web_project1.application.usecase.GymUseCase;
 import com.example.gym_web_project1.domain.Gym;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @WebMvcTest(GymController.class)
